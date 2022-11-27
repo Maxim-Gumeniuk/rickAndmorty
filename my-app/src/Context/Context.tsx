b4d6c4a1d3814a33903e64b7ba97af6966eeda5b
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-11-28 00:46:25 +0200

    second

--- my-app/src/Context/Context.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const initialValue = (param: number) => {
-  () => {}; // eslint-disable-line @typescript-eslint/no-empty-function
-}; // eslint-disable-line @typescript-eslint/no-empty-function
-export const Context = React.createContext(initialValue); // eslint-disable-line @typescript-eslint/no-empty-function
